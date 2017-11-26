688cef9767e4721a0b3efddce80a6a2bd5d802c4
K Ghosh <kghosh.ns@gmail.com>
2017-11-26 16:51:59 -0500

    add code to testWaitingState

--- src/test/InputHandlerTest.java
+++ src/test/InputHandlerTest.java
@@ -2,14 +2,32 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import server.logic.handler.InputHandler;
+import server.logic.handler.model.Output;
+import server.logic.handler.model.ServerOutput;
 
 
 public class InputHandlerTest {
 	
+	public static final int WAITING = 0;
+	InputHandler inputHandler;
+	ServerOutput serverOutput;
+	Output output;
+	
+	@Before
+	public void setUp(){
+		output = new Output("",0);
+		inputHandler = new InputHandler();
+		serverOutput = new ServerOutput("",output.getState());
+	}
+	
 	@Test
 	public void testWaitingState() {
-		fail("Not yet implemented");
+		serverOutput = inputHandler.processInput("", WAITING);
+		assertTrue(serverOutput.getOutput().equalsIgnoreCase("Who Are you?Clerk or Student?"));
 	}
 
 }
