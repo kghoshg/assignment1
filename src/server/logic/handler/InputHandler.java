8e76a833fb961a7d4d3d5212cef07be254d83f3e
K Ghosh <kghosh.ns@gmail.com>
2017-11-26 17:03:04 -0500

    modified processinput()

--- src/server/logic/handler/InputHandler.java
+++ src/server/logic/handler/InputHandler.java
@@ -8,6 +8,7 @@ public class InputHandler {
 	public static final int WAITING = 0;
 	public static final int FINISHWAITING=1;
 	public static final int CLERKLOGIN=2;
+	public static final int STUDENTLOGIN=3;
 
 	public ServerOutput processInput(String input, int state) {
 		 String output = "";
@@ -23,6 +24,16 @@ public class InputHandler {
 		            	output="Please Input The Password:";
 		            	state=CLERKLOGIN;
 		                oo.setOutput(output);
+			            oo.setState(state);
+		            }else if (input.equalsIgnoreCase("student")) {
+		            	output="Please Input Username and Password:'username,password'";
+		            	state=STUDENTLOGIN;
+		                oo.setOutput(output);
+			            oo.setState(state);
+		            }else{
+		            	output = "Who Are you?Clerk or Student?";
+		            	state = FINISHWAITING;
+		            	oo.setOutput(output);
 			            oo.setState(state);
 		            }
 	         }
