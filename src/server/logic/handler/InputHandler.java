42553551b7a2fdac4b932d265ef3c45b5511eb94
K Ghosh <kghosh.ns@gmail.com>
2017-11-26 17:20:23 -0500

    add code for clerk log in

--- src/server/logic/handler/InputHandler.java
+++ src/server/logic/handler/InputHandler.java
@@ -9,7 +9,9 @@ public class InputHandler {
 	public static final int FINISHWAITING=1;
 	public static final int CLERKLOGIN=2;
 	public static final int STUDENTLOGIN=3;
-
+	
+	OutputHandler outputHandler=new OutputHandler();
+	
 	public ServerOutput processInput(String input, int state) {
 		 String output = "";
 		 Output o = new Output("",0);
@@ -36,7 +38,13 @@ public class InputHandler {
 		            	oo.setOutput(output);
 			            oo.setState(state);
 		            }
-	         }
+	         }else if(state==CLERKLOGIN){
+		        	o=outputHandler.clerkLogin(input);
+	        		output=o.getOutput();
+	        		state=o.getState();
+	        		oo.setOutput(output);
+		            oo.setState(state);
+		     }
 		return oo;
 	}
 }
--- src/server/logic/handler/OutputHandler.java
+++ src/server/logic/handler/OutputHandler.java
@@ -1,5 +1,23 @@
 package server.logic.handler;
 
+import server.logic.handler.model.Output;
+import utilities.Config;
+
 public class OutputHandler {
+	
+	public static final int CLERKLOGIN=2;
+	 public static final int CLERK = 3;
+	
+	public Output clerkLogin(String input) {
+		Output output=new Output("",0);
+		if(input.equalsIgnoreCase(Config.CLERK_PASSWORD)){
+			output.setOutput("What can I do for you?Menu:Create Student/Course, Delete Student/Course, Register/Cancel/Destroy Course, List /Students/Courses");
+        	output.setState(CLERK);
+		}else{
+			output.setOutput("Wrong Password!Please Input The Password:");
+        	output.setState(CLERKLOGIN);
+		}
+		return output;
+	}
 
 }
