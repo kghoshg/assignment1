106030236c03294a135598a28844a6b643b7e8a3
K Ghosh <kghosh.ns@gmail.com>
2017-11-26 16:54:23 -0500

    add code to processInput for initial message for user

--- src/server/logic/handler/InputHandler.java
+++ src/server/logic/handler/InputHandler.java
@@ -1,12 +1,23 @@
 package server.logic.handler;
 
+import server.logic.handler.model.Output;
 import server.logic.handler.model.ServerOutput;
 
 public class InputHandler {
 	
 	public static final int WAITING = 0;
+	public static final int FINISHWAITING=1;
 
 	public ServerOutput processInput(String input, int state) {
-		return null;
+		 String output = "";
+		 Output o = new Output("",0);
+		 ServerOutput oo = new ServerOutput(output,o.getState());
+	        if (state == WAITING) {
+	        	output = "Who Are you?Clerk or Student?";
+	            state = FINISHWAITING;
+	            oo.setOutput(output);
+	            oo.setState(state);
+	         }
+		return oo;
 	}
 }
